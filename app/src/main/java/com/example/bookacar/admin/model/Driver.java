66fb078f22f61bb3fb9065b5300baaf79d0459ff
MinhTung-99 <58562847+MinhTung-99@users.noreply.github.com>
2021-09-26 00:52:43 +0700

    push code

---
+++ app/src/main/java/com/example/bookacar/admin/model/Driver.java
@@ -0,0 +1,49 @@
+package com.example.bookacar.admin.model;
+
+public class Driver {
+    private String id;
+    private String image;
+    private String name;
+    private String password;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
