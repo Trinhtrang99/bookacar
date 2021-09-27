feb35f17d1d1e5a44ba9fcdbc9f7cb554b2caff3
MinhTung-99 <58562847+MinhTung-99@users.noreply.github.com>
2021-09-27 11:30:49 +0700

    push code

--- app/src/main/java/com/example/bookacar/util/Constants.java
+++ app/src/main/java/com/example/bookacar/util/Constants.java
@@ -6,13 +6,15 @@ public class Constants {
     public final static String KEY_PASSWORD = "password";
     public final static String KEY_COLLECTION_ACCOUNT = "accounts";
     public final static String KEY_TYPE_USER = "type";
-    public final static String TYPE_DRIVER = "driver";
+    public final static String TYPE_DRIVER = "Tài khoản dành cho tài xế";
     public final static String TYPE_ADMIN = "admin";
     public final static String KEY_NAME = "name";
     public final static String KEY_IMAGE = "image";
     public final static String KEY_COLLECTION_BOOK = "books";
     public final static String KEY_LOCATION_START = "locationStart";
     public final static String KEY_LOCATION_END = "locationEnd";
+    public final static String KEY_COLLECTION_USER_DRIVER = "userDriver";
+    public final static String KEY_CONFIRM_USER_DRIVER = "confirmUserDriver";
 
     public final static String KEY_PREFERENCE_NAME = "preference";
     public final static String KEY_IS_REMEMBER_PASSWORD = "remember_password";
